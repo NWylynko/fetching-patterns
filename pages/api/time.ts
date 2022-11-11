0b8a8792e44a4a444d270650bd98533541fd60d2
Nick Wylynko <nick@wylynko.com>
2022-11-12 00:22:55 +0800

    switch back to old api (req, res) api

--- pages/api/time.ts
+++ pages/api/time.ts
@@ -1,6 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function GetTime(req: Request) {
-  return new Response(JSON.stringify({
+
+export default function GetTime(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
     timestamp: new Date()
-  }))
+  })
 }
