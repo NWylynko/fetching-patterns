b23118f05efc9934931770d16b50870bf392ba08
Nick Wylynko <nick@wylynko.com>
2022-11-12 11:25:05 +0800

    Add in No Store

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <DefaultAsyncFetch />
       <ForceCacheAsyncFetch />
       <NoCacheAsyncFetch />
-      {/* <NoStoreAsyncFetch /> */}
+      <NoStoreAsyncFetch />
       {/* <OnlyIfCachedAsyncFetch /> */}
       {/* <ReloadAsyncFetch /> */}
     </div>
