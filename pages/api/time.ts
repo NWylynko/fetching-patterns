c0e8d33da056fe5197e3f6bef99360ea46198a7e
Nick Wylynko <nick@wylynko.com>
2022-11-12 11:41:38 +0800

    return the type from the user in the response

--- pages/api/time.ts
+++ pages/api/time.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 export default function GetTime(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
+    type: req.query.type,
     timestamp: new Date()
   })
 }
