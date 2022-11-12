d09a6281f0ba842271ea458335e51e8255b0f0bc
Nick Wylynko <nick@wylynko.com>
2022-11-12 12:46:09 +0800

    Add details to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default async function Home() {
   return (
     <>
-      <span>Click on one of the links above</span>
+      <h4>Click on one of the links above</h4>
+      <a href="https://github.com/nwylynko/fetching-patterns" style={{ color: "black", display: "block", marginBottom: 8 }}>Github Repo</a>
+      <span>Versions</span>
+      <ul>
+        <li>Next 13.0.3</li>
+        <li>React 18.2.0</li>
+      </ul>
+      <span>Next Config</span>
+      <pre>{JSON.stringify({
+          experimental: {
+            appDir: true,
+          }
+        }, null, 2)}</pre>
     </>
   )
 }
